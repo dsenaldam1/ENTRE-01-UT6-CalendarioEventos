e921ac7bcb4b92e8828a271817c713d59b902109
David Sena <dsenaldam1@gmail.com>
2021-02-19 16:36:57 +0100

    Clase mes añadida

---
+++ Mes.java
@@ -0,0 +1,12 @@
+
+/**
+ * Enumeration class Mes - write a description of the enum class here
+ * 
+ * @author David Sena
+ * @version (version number or date here)
+ */
+public enum Mes
+{
+    ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO,JULIO, AGOSTO,
+    SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE;
+}
